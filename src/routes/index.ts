e5c5d8cf3900c7a7ff35207183b990215629a161
HOANGPHI <phi.le@gumiviet.com>
2022-07-21 17:30:18 +0700

    add middleware auth

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import { Register , Login, AuthenticatedUser, Logout} from "../controllers/auth.controller";
+import {AuthMiddleware} from "../middlewares/auth.middleware";
 
 
 export const routes =async (router: Router)=>{
     router.post('/api/register', Register);
     router.post('/api/login', Login);
-    router.post('/api/logout',Logout)
-    router.get('/api/user', AuthenticatedUser)
+    router.post('/api/logout', AuthMiddleware,Logout)
+    router.get('/api/user',AuthMiddleware, AuthenticatedUser)
 }
